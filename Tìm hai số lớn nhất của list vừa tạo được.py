515b1fb7508f57f2ce6822c1acc522a21bb526f0
nguyenbaongoc123 <127759913+nguyenbaongoc123@users.noreply.github.com>
2023-04-21 05:09:56 +0900

    Create Tìm hai số lớn nhất của list vừa tạo được.py

---
+++ Tìm hai số lớn nhất của list vừa tạo được.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+lists = [15,20,2,65,85,99,33,100,44,57]
+lists.sort()
+print("hai số lớn nhất của list vừa tạo được : ")
+print(lists[-1], lists[-2])
+
+
+# In[ ]:
+
+
+
+
